0d7a6f67038fa095c262f2432896c473bcec4fac
MuneerHamdan <muneerhamdan01@gmail.com>
2024-04-04 14:49:06 -0400

    first commit

--- src/photos/Photos.java
+++ src/photos/Photos.java
@@ -1,8 +1,29 @@
 // Muneer Hamdan myh16, Gabriel Ignacio
 package photos;
 
-public class Photos{
-    public static void main(String[]args){
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Photos extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    
+        StackPane root = new StackPane();
+
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Photo Library");
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
