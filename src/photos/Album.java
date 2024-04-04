1310ed1c6be324842fa24b70966df6e00b08bf37
MuneerHamdan <muneerhamdan01@gmail.com>
2024-04-04 15:54:38 -0400

    album

---
+++ src/photos/Album.java
@@ -0,0 +1,6 @@
+package photos;
+
+public class Album {
+
+    
+}
--- src/photos/User.java
+++ src/photos/User.java
@@ -1,9 +1,11 @@
 package photos;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class User implements Serializable{
     private String username;
+    private ArrayList<Album> albums;
 
     public User(String name){
         username = name;
@@ -12,5 +14,26 @@ public class User implements Serializable{
     public String getUsername(){
         return username;
     }
+
+    //create albums
+    public void createAlbum(){
+        albums.add(new Album());
+    }
+
+    //delete albums
+    public void deleteAlbum(){
+
+    }
+
+    //rename albums
+    public void renameAlbum(){
+
+    }
+    
+    //open album
+
+    //search for photos
+
+    //logout
     
 }
