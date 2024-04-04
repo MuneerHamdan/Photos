3004c4ad8e53b0debd72eddb50064042a8482048
MuneerHamdan <muneerhamdan01@gmail.com>
2024-04-04 15:14:55 -0400

    admin

---
+++ src/photos/Admin.java
@@ -0,0 +1,31 @@
+package photos;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class Admin {
+   
+    private static ArrayList<User> users = new ArrayList<>();
+
+    //list users
+    public static Iterator<User> listUsers(){
+       return users.iterator(); 
+    }
+
+    //create new user
+    public static void createUser(String username){
+        users.add(new User(username));
+    }
+
+    //delete existing user
+    public static void deleteUser(String username){
+        
+        User u = users.getFirst(); 
+        
+        for (User user: users){
+            if (user.getUsername().equalsIgnoreCase(username)) u = user;
+        }
+
+        users.remove(u);
+    }
+}
