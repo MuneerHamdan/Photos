ba4e731d02bc2a9ddac76e0dc37f7e2555c2685e
MuneerHamdan <muneerhamdan01@gmail.com>
2024-04-04 21:25:34 -0400

    photo

--- src/photos/Photo.java
+++ src/photos/Photo.java
@@ -1,5 +1,29 @@
 package photos;
 
-public class Photo{
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Photo implements Serializable{
+
+    //tags
+    //looks like hashmap key value pairs
+    //add custom tag type list later
+    private Map<String, ArrayList<String>> tag = new HashMap<>();
+
+    //date serializable
+
+    // stock in workspace, user not
+    // >5 < 10 stock photos
+    //username 'stock' pass=stock, album stock
+
+    //caption
+    private String caption = "";
+
+    public Photo(){
+        
+
+    }
 
 }
--- src/photos/User.java
+++ src/photos/User.java
@@ -3,50 +3,51 @@ package photos;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class User implements Serializable{
+public class User implements Serializable {
     private String username;
     private ArrayList<Album> albums;
 
-    public User(String name){
+    public User(String name) {
         username = name;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
-    //create albums
-    public void createAlbum(){
+    // create albums
+    public void createAlbum() {
         albums.add(new Album());
     }
 
-    //get albums list
-    public ArrayList<Album> getAlbums(){
+    // get albums list
+    public ArrayList<Album> getAlbums() {
         return albums;
     }
 
-    //get specific album by name
-    public Album getAlbum(String name){
-        for (Album album : albums){
-            if (album.getName().equalsIgnoreCase(name)) return album;
+    // get specific album by name
+    public Album getAlbum(String name) {
+        for (Album album : albums) {
+            if (album.getName().equalsIgnoreCase(name))
+                return album;
         }
         return null;
     }
 
-    //delete albums
-    public void deleteAlbum(Album album){
+    // delete albums
+    public void deleteAlbum(Album album) {
         albums.remove(album);
     }
 
-    //rename albums
-    public void renameAlbum(Album album, String name){
+    // rename albums
+    public void renameAlbum(Album album, String name) {
         album.rename(name);
     }
-    
-    //open album
 
-    //search for photos
+    // open album
 
-    //logout
-    
+    // search for photos
+
+    // logout
+
 }
